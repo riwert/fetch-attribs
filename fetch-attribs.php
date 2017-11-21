64700dbf3e0485ed679e58108103979776684894
Riwert <revert@o2.pl>
2017-11-21 11:39:47 +0100

    Update fetch-attribs.php

--- fetch-attribs.php
+++ fetch-attribs.php
@@ -4,7 +4,14 @@ $url = ( ! empty($argv[1])) ? $argv[1] : 'https://google.pl';
 $tag = ( ! empty($argv[2])) ? $argv[2] : 'img';
 $attr = ( ! empty($argv[3])) ? $argv[3] : 'src';
 
-$html = file_get_contents($url);
+$contextOptions = [
+    'ssl' => [
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+    ]
+];
+
+$html = file_get_contents($url, false, stream_context_create($contextOptions));
 
 $doc = new DOMDocument();
 @$doc->loadHTML($html);
@@ -12,7 +19,8 @@ $doc = new DOMDocument();
 $tags = $doc->getElementsByTagName($tag);
 
 foreach ($tags as $tag) {
-       $attribute = $tag->getAttribute($attr);
-       if ($attribute)
-       		echo $attribute . "\n";
+	$attribute = $tag->getAttribute($attr);
+	if ($attribute) {
+		echo $attribute . "\n";
+	}
 }
